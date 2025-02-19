f7cb18785d0cef11818d4ad649954e82542dac69
namig <namig.nurmamedov@yandex.ru>
2025-02-19 20:00:57 +0700

    Fix package.json

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -12,6 +13,12 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
+  const defaultOptions = new DocumentBuilder().setTitle('OVPN API').build();
+  const defaultDocument = SwaggerModule.createDocument(app, defaultOptions, {
+    include: [],
+  });
+  SwaggerModule.setup('api/doc', app, defaultDocument);
 
   await app.listen(process.env.PORT ?? 3000);
 }
